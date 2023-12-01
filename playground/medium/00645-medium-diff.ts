c1a169aedd6b795ca609bc0b3a869bb3d082a8a4
Seungwoo321 <seungwoo321@gmail.com>
2023-12-01 17:30:25 +0900

    feat: solve 00645

--- playground/medium/00645-medium-diff.ts
+++ playground/medium/00645-medium-diff.ts
@@ -12,7 +12,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Diff<O, O1> = any
+type Diff<O, O1> = Omit<O & O1, keyof (O | O1)>
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
