085e1806c8b575d293cc36655fb5874d7ab87ab1
Seungwoo321 <seungwoo321@gmail.com>
2025-06-10 13:52:28 +0900

    ac: medium 00949

--- playground/medium/00949-medium-anyof.ts
+++ playground/medium/00949-medium-anyof.ts
@@ -23,8 +23,17 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type AnyOf<T extends readonly any[]> = any
+// type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+//   ? F extends 0 | '' | false | [] | Record<string, never> | undefined | null
+//     ? AnyOf<R>
+//     : true
+//   : false
 
+type AnyOf<T extends readonly any[]> = T[number] extends 
+  0 | '' | false | [] | Record<string, never> | undefined | null
+  ? false
+  : true
+  
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
